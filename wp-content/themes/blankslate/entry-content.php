3d6ff1b9761f9542e0f6a29ea40deee007fa1ff1
Viet Nguyen <viet.nguyen@fujitechjsc.com>
2017-10-11 18:32:29 +0700

    add  p tag

--- wp-content/themes/blankslate/entry-content.php
+++ wp-content/themes/blankslate/entry-content.php
@@ -1,5 +1,7 @@
 <section class="entry-content">
+<p>
 <?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
+</p>
 <?php the_content(); ?>
 <div class="entry-links"><?php wp_link_pages(); ?></div>
 </section>
